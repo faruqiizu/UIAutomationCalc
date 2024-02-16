95bcf865c0a29abd6d165277391789de2b8761d8
kudof <izzufaruqi@gmail.com>
2024-02-16 15:27:35 +0700

    new commit list

--- UIAutomationCalc/src/test/java/com/example/steps/ListPageSteps.java
+++ UIAutomationCalc/src/test/java/com/example/steps/ListPageSteps.java
@@ -2,6 +2,7 @@ package com.example.steps;
 
 import com.example.BaseTest;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 
 public class ListPageSteps extends BaseTest {
 
@@ -10,4 +11,15 @@ public class ListPageSteps extends BaseTest {
     listPage.swipeList();
   }
 
+    @When("user long press list button")
+    public void userLongPressListButton() {
+    }
+
+    @And("user see list menu")
+    public void userSeeListMenu() {
+    }
+
+    @When("user multiple press list button")
+    public void userMultiplePressListButton() {
+    }
 }
